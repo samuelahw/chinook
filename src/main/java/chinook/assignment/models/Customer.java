455af276855fb10ef36c61eba122d87d458faef7
samuelahw <samuel.ahjoniemi@gmail.com>
2023-02-23 12:50:58 +0200

    fix Javadoc

--- src/main/java/chinook/assignment/models/Customer.java
+++ src/main/java/chinook/assignment/models/Customer.java
@@ -1,5 +1,16 @@
 package chinook.assignment.models;
 
+/**
+ * A model for customer, contains data of customer.
+ *
+ * @param customerId int, id of customer
+ * @param firstName  String, first name
+ * @param lastName   String, last name
+ * @param country    String, country name
+ * @param postalCode String, postal code
+ * @param phone      String, phone number
+ * @param email      String, email
+ */
 public record Customer(int customerId, String firstName, String lastName, String country, String postalCode,
                        String phone, String email) {
 }
